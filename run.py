49504a9746f6aacb1a5222c57219e2a4748a0bc7
Jack Gerrits <jack@jackgerrits.com>
2022-01-31 12:40:15 -0500

    rewrite tool to use PyGithub and be usable as a CLI tool

--- run.py
+++ run.py
@@ -1,27 +1,103 @@
-from ghapi.all import GhApi
+from typing import List
+import github
+import github.GithubException
 import requests
 import zipfile
 import io
 from requests.auth import HTTPBasicAuth
+import argparse
 
-USERNAME = ""
-TOKEN = "" # Requires actions scope
-BRANCH = ""
 
-api = GhApi(owner='VowpalWabbit', repo='vowpal_wabbit',token=TOKEN)
-runs_for_branch = api.actions.list_workflow_runs_for_repo(branch=BRANCH).workflow_runs
-commit_id_for_most_recent_run = runs_for_branch[0].head_commit.id
-runs_for_commit_id = [x for x in runs_for_branch if x.head_commit.id == commit_id_for_most_recent_run]
+class Artifact:
+    name: str
+    url: str
 
-macos_wheel_run = next(x for x in runs_for_commit_id if x.name == "Build MacOS Python Wheels")
-linux_wheel_run = next(x for x in runs_for_commit_id if x.name == "Build Linux Python Wheels")
-windows_wheel_run = next(x for x in runs_for_commit_id if x.name == "Build Windows Python Wheels")
-artifacts = []
-artifacts.extend(api.actions.list_workflow_run_artifacts(macos_wheel_run.id).artifacts)
-artifacts.extend(api.actions.list_workflow_run_artifacts(linux_wheel_run.id).artifacts)
-artifacts.extend(api.actions.list_workflow_run_artifacts(windows_wheel_run.id).artifacts)
+    def __init__(self, name: str, url: str):
+        self.name = name
+        self.url = url
 
-for artifact in artifacts:
-    r = requests.get(artifact.archive_download_url,  auth=HTTPBasicAuth(USERNAME, TOKEN))
-    z = zipfile.ZipFile(io.BytesIO(r.content))
-    z.extractall()
+    def download(self, dest_dir: str, username: str, token: str):
+        r = requests.get(self.url, auth=HTTPBasicAuth(username, token))
+        z = zipfile.ZipFile(io.BytesIO(r.content))
+        z.extractall(dest_dir)
+
+
+def get_artifacts_for_run(
+    run: github.WorkflowRun, username: str, token: str
+) -> List[Artifact]:
+    artifacts = []
+    response = requests.get(
+        run.artifacts_url, auth=HTTPBasicAuth(username, token)
+    ).json()
+    for artifact in response["artifacts"]:
+        artifacts.append(Artifact(artifact["name"], artifact["archive_download_url"]))
+    return artifacts
+
+
+if __name__ == "__main__":
+    DEFAULT_WORKFLOWS = "build_python_wheels.yml,build_python_wheels_aarch64.yml,build_python_wheels_macos.yml,build_python_wheels_windows.yml"
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument("--username", type=str, required=True)
+    parser.add_argument("--token", type=str, required=True)
+    parser.add_argument("--commit", type=str, required=True)
+    parser.add_argument(
+        "--out_dir",
+        type=str,
+        default="out",
+        help="Destination dir of downloaded artifacts",
+    )
+    parser.add_argument(
+        "--max_lookback",
+        type=int,
+        default=100,
+        help="How many runs backwards to search for the desired commit",
+    )
+    parser.add_argument(
+        "--repo",
+        type=str,
+        default="Vowpalwabbit/vowpal_wabbit",
+        help="Github account and repo to search",
+    )
+    parser.add_argument(
+        "--workflows",
+        type=str,
+        default=DEFAULT_WORKFLOWS,
+        help="Comma separated list of workflows to download artifacts for",
+    )
+    args = parser.parse_args()
+
+    workflows_to_process = args.workflows.split(",")
+
+    api = github.Github(args.username, args.token)
+    repo = api.get_repo(args.repo)
+    found_workflow_runs = []
+    for workflow in workflows_to_process:
+        try:
+            found_workflow = repo.get_workflow(workflow)
+        except github.GithubException as e:
+            raise ValueError(f"Unknown workflow '{workflow}'")
+
+        found_run = None
+        for run in found_workflow.get_runs():
+            if run.head_sha == args.commit:
+                print("Found run for {}".format(workflow))
+                found_run = run
+                break
+        if found_run is None:
+            raise ValueError(
+                f"Could not find run for {args.commit} in {workflow}. You can try increasing max lookback."
+            )
+
+        found_workflow_runs.append((workflow, found_run))
+
+    found_artifacts = []
+    for name, run in found_workflow_runs:
+        artifacts = get_artifacts_for_run(run, args.username, args.token)
+        found_artifacts.extend(artifacts)
+        print("Discovered {} artifacts for {}".format(len(artifacts), name))
+
+    for artifact in found_artifacts:
+        print(f"Downloading {artifact.name}...")
+        artifact.download(args.out_dir, args.username, args.token)
